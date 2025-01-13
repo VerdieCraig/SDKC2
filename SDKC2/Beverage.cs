bf9894c12508400b0ab49f3e1c85203809634063
Verdie Craig <craigverdie@gmail.com>
2025-01-13 00:03:46 -0500

    I did it!

---
+++ SDKC2/Beverage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDKC2
+{
+    internal class Beverage : Product
+    {
+        public string? Coffee { get; set; }
+
+
+    }
+}
---
+++ SDKC2/Program.cs
@@ -0,0 +1,37 @@
+﻿using SDKC2;
+using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<Beverage>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var beverage = new Beverage();
+   
+    Console.WriteLine("Creating a beverage...");
+
+    Console.Write("What is the name of the beverage? ");
+    Beverage.Name = Console.ReadLine();
+
+    Console.Write("Is the beverage coffee? ");
+    Beverage.Coffee = Console.ReadLine();
+
+    Console.Write("Briefly describe the beverage ");
+    Beverage.Description = Console.ReadLine();
+
+    Console.Write("What does the beverage cost?");
+    Beverage.Price = decimal.Parse(Console.ReadLine());
+
+    Console.Write("How many beverages are you adding? ");
+    Beverage.Quantity = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Added a beverage");
+    
+    recordList.Add(item: Beverage);
+
+    }
+public override string ToString();
+
+// Print out the list of records using Console.WriteLine()
